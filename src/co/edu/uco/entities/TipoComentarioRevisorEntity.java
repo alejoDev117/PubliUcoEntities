5ace5c49bc25b8b70a0b0c0ed178f0ea0fccdf62
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2023-05-01 02:52:18 -0500

    se crean mas entities

---
+++ src/co/edu/uco/entities/TipoComentarioRevisorEntity.java
@@ -0,0 +1,71 @@
+ package co.edu.uco.entities;
+
+import java.util.UUID;
+
+import co.edu.uco.crosscutting.utils.UtilText;
+import co.edu.uco.crosscutting.utils.UtilUUID;
+
+public class TipoComentarioRevisorEntity {
+	
+	private static final TipoComentarioRevisorEntity DEFAULT_OBJECT = new TipoComentarioRevisorEntity();
+	private UUID identificador;
+	private String nombre;
+	private String descripcion;
+	
+	
+	
+	public TipoComentarioRevisorEntity(UUID identificador, String nombre, String descripcion) {
+		super();
+		setIdentificador(identificador);
+		setNombre(nombre);
+		setDescripcion(descripcion);	
+		}
+	
+	private  TipoComentarioRevisorEntity() {
+		super();
+		setIdentificador(UtilUUID.getDefaultUuid());
+		setNombre(UtilText.getUtilText().getDefaultValue());
+		setDescripcion(UtilText.getUtilText().getDefaultValue());
+	}
+	
+	
+	public static TipoComentarioRevisorEntity getDefaultObject() {
+		return DEFAULT_OBJECT;
+	}
+
+	
+	
+	public final UUID getIdentificador() {
+		return identificador;
+	}
+	private final void setIdentificador(final UUID identificador) {
+		this.identificador = UtilUUID.getDefault(identificador); 
+		
+
+	}
+	public final String getNombre() {
+		return nombre;
+	}
+	
+	
+	private final void setNombre(final String nombre) {
+		this.nombre = UtilText.getUtilText().applyTrim(nombre);
+	
+		
+	}
+	
+	public final String getDescripcion() {
+		return descripcion;
+	}
+	
+	
+	private final void setDescripcion(final String descripcion) {
+		this.descripcion = UtilText.getUtilText().applyTrim(nombre);
+		
+	
+	}
+
+
+
+}
+
